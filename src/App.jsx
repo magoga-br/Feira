85c97bedb4463a8f6b2923dfbca0bd5e723eeb9d
fabricio magoga <falolmagoga@gmail.com>
2025-08-24 19:29:49 -0300

    refactor: remove unused CSS files and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Produtos from "./pages/produtos"
+import Login from "./pages/Login";
+import Index from "./pages/Index";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Header />
+      
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/produtos" element={<Produtos />} />
+         <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      <Footer />
+
+    </Router>
   )
 }
 
